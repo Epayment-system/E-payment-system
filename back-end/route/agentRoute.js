b6500af35ca10c6167db0141be7b6ea71ce7cd56
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-04 12:23:37 +0100

    route folder

---
+++ back-end/route/agentRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const agentRouter = express.Router();
+const AgentController = require('../controller/agentController.js');
+
+
+agentRouter.post('/',AgentController.create)
+agentRouter.get('/', AgentController.findAll)
+agentRouter.get('/:id', AgentController.findOne)
+agentRouter.put('/:id', AgentController.update)
+agentRouter.delete('/:id', AgentController.delete)
+
+module.exports= agentRouter
+
+
+const express = require('express');
+const serviceProvidersRouter = express.Router();
+const serviceProviderController = require('../controller/serviceproviderController');
+
+
+
+
+
+
