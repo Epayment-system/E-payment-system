fdd679113b6a2d01d139b880d5ab73de6ed9bf38
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-04 12:12:36 +0100

    agent model

---
+++ back-end/models/agentModel.js
@@ -0,0 +1,44 @@
+module.exports=(sequelize,DataTypes)=>{
+    const Agents = sequelize.define("Agents",{
+        agentBIN:{
+            type: DataTypes.INTEGER,
+            allowNUll:false,
+            primarykey: true,
+        },
+
+        agentName:{
+            type: DataTypes.STRING,
+            allowNUll:false
+        },
+
+        agentEmail:{
+            type: DataTypes.STRING,
+            allowNUll:false
+        },
+
+        agentPassword:{
+            type: DataTypes.STRING,
+            allowNUll:false
+        },
+
+        servicesOffered:{
+            type: DataTypes.STRING,
+            allowNUll:false
+        },
+
+
+        phoneNumber:{
+            type: DataTypes.STRING,
+            allowNUll:false
+        },
+
+        agentAuthorizationLetter: {
+            type: DataTypes.BLOB,
+            allowNull: false
+        }
+
+
+    })
+    return Agents;
+}
+
