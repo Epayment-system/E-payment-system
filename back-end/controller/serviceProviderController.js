17a5070d1da15c0fa8434040cb4bd70b7a154ec7
EftuTesfaye <88849233+EftuTesfaye@users.noreply.github.com>
2023-08-09 13:36:12 +0300

    Update serviceProviderController.js

--- back-end/controller/serviceProviderController.js
+++ back-end/controller/serviceProviderController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require('express-async-handler');
 const db = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const ServiceProviders = db.ServiceProviders;
 
@@ -7,50 +10,58 @@ const ServiceProviders = db.ServiceProviders;
 exports.create = asyncHandler(async (req, res) => {
   // Validate request
 
-  if (!req.body. serviceProviderBIN || !req.body. serviceProviderName||
-     !req.body. serviceProviderEmail || !req.body. serviceProviderPassword || 
-     !req.body.servicesOffered || !req.body.BankName
-     || !req.body. BankAccountNumber|| !req.body.phoneNumber 
-      || !req.body. serviceProviderAuthorizationLetter ) {
+  if (
+    !req.body.serviceProviderBIN ||
+    !req.body.serviceProviderName ||
+    !req.body.serviceProviderEmail ||
+    !req.body.serviceProviderPassword ||
+    !req.body.servicesOffered ||
+    !req.body.BankName ||
+    !req.body.BankAccountNumber ||
+    !req.body.phoneNumber ||
+    !req.body.serviceProviderAuthorizationLetter
+  ) {
     res.status(400).send({
-      message: (' serviceProviderBIN,  serviceProviderName,  serviceProviderEmail, serviceProviderPassword, servicesOffered and phoneNumber cannot be empty'),
+      message:
+        'serviceProviderBIN, serviceProviderName, serviceProviderEmail, serviceProviderPassword, servicesOffered, BankName, BankAccountNumber, phoneNumber, and serviceProviderAuthorizationLetter cannot be empty',
+    });
+    return; 
+  }
+  //Check if user already exists
+  const existingServiceProvider= await ServiceProviders.findOne({
+    where: {
+      serviceProviderEmail: req.body.serviceProviderEmail,
+    },
+  });
+
+  if (existingServiceProvider) {
+    res.status(409).send({
+      message: 'Service provider already exists',
     });
     return;
   }
 
-  // Create an agent object
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(req.body.serviceProviderPassword, 10);
+  // Create a service provider object
   const services = {
-    serviceProviderBIN: req.body. serviceProviderBIN,
-    serviceProviderName: req.body. serviceProviderName,
-    serviceProviderEmail: req.body. serviceProviderEmail,
-    serviceProviderPassword: req.body. serviceProviderPassword,
+    serviceProviderBIN: req.body.serviceProviderBIN,
+    serviceProviderName: req.body.serviceProviderName,
+    serviceProviderEmail: req.body.serviceProviderEmail,
+    serviceProviderPassword:hashedPassword,
     servicesOffered: req.body.servicesOffered,
-    BankName: req.body. BankName,
+    BankName: req.body.BankName,
     BankAccountNumber: req.body.BankAccountNumber,
     phoneNumber: req.body.phoneNumber,
-    serviceProviderAuthorizationLetter: req.body. serviceProviderAuthorizationLetter
+    serviceProviderAuthorizationLetter: req.body.serviceProviderAuthorizationLetter,
   };
- /* if (!req.body.Services_id || !req.body.Services_name || !req.body.Services_description || !req.body.Services_contact ) {
-    res.status(400).send({
-      message: ('Services_id, Services_name, Services_description, and Services_contact cannot be empty'),
-    });
-    return;
-  }
 
-  // Create a services object
-  const services = {
-    Services_id: req.body.Services_id,
-    Services_name: req.body.Services_name,
-    Services_description: req.body.Services_description,
-    Services_contact: req.body.Services_contact
-  };*/
-
-  // Save services in the database
+  // Save service provider in the database
   const data = await ServiceProviders.create(services);
   res.send(data);
 });
 
-// Retrieve all services from the database
+// Retrieve all service providers from the database
 exports.findAll = asyncHandler(async (req, res) => {
   const data = await ServiceProviders.findAll();
   res.send(data);
@@ -63,7 +74,7 @@ exports.findOne = asyncHandler(async (req, res) => {
   const data = await ServiceProviders.findByPk(id);
   if (!data) {
     res.status(404).send({
-      message: (`ServiceProviders with id=${id} not found`),
+      message: `ServiceProviders with id=${id} not found`,
     });
   } else {
     res.send(data);
@@ -84,7 +95,7 @@ exports.update = asyncHandler(async (req, res) => {
     });
   } else {
     res.send({
-      message: (`Cannot update ServiceProviders with id=${id}. ServiceProviders not found or req.body is empty!`),
+      message: `Cannot update ServiceProviders with id=${id}. ServiceProviders not found or req.body is empty!`,
     });
   }
 });
@@ -103,7 +114,43 @@ exports.delete = asyncHandler(async (req, res) => {
     });
   } else {
     res.send({
-      message: (`Cannot delete ServiceProviders with id=${id}. ServiceProviders not found!`),
+      message: `Cannot delete ServiceProviders with id=${id}. ServiceProviders not found!`,
     });
+  }
+});
+
+// Service provider login auth
+exports.login = asyncHandler(async (req, res) => {
+  try {
+    const { serviceProviderEmail, serviceProviderPassword } = req.body;
+    const serviceProvider = await ServiceProviders.findOne({
+      where: { serviceProviderEmail },
+    });
+
+    if (!serviceProvider) {
+      res.status(404).json({ error: 'Service provider not found' });
+    } else {
+      const passwordMatch = await bcrypt.compare(
+        serviceProviderPassword,
+        serviceProvider.serviceProviderPassword
+      );
+
+      if (!passwordMatch) {
+        res.status(401).json({ error: 'Incorrect password' });
+      } else {
+        const token = jwt.sign(
+          { serviceProviderBIN: serviceProvider.id },
+          process.env.TOKEN_SECRET,
+          {
+            expiresIn: '1h',
+          }
+        );
+        res.status(200).json({ message: 'Login successful', token });
+      }
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Failed to login service provider', message: error.message });
   }
 });
