4fc079db7db215446770c54e00b5ea0c269506f5
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-10 19:50:44 +0100

    Update agentRoute.js

--- back-end/route/agentRoute.js
+++ back-end/route/agentRoute.js
@@ -5,9 +5,8 @@ const AgentController = require('../controllers/agentController.js');
 const agentRouter = express.Router();
 
 // Define routes for Agents
-//agentRouter.post('/register',AgentController.agentRegistration)
+agentRouter.post('/',AgentController.upload,AgentController.create)
 agentRouter.post('/login',AgentController.agentLogin)
-agentRouter.post('/',AgentController.create)
 agentRouter.get('/', AgentController.findAll)
 agentRouter.get('/:id', AgentController.findOne)
 agentRouter.put('/:id', AgentController.update)
