e94a14da55b1ae57bc9439e9465c9fe1004f369c
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-10 20:47:20 +0100

    Update index.js

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -1,43 +1,101 @@
-'use strict';
+"use strict";
+const { Model, Sequelize } = require('sequelize');
+const dbConfig = require('../config/dbconfig.js');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
   }
-});
+);
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.Bill = require('./billModel.js')(sequelize, Sequelize);
+db.Agents = require('./agentModel.js')(sequelize, Sequelize);
+db.User = require('./userModel.js')(sequelize, Sequelize);
+db.ServiceProviders = require('./serviceProviderModel.js')(sequelize, Sequelize);
+db.payment = require('./paymentModel.js')(sequelize, Sequelize);
+// db.AgentHistory = require('./agentHistoryModel.js')(sequelize, Sequelize);
+// db.ServiceProviderHistory = require('./serviceProviderHistoryModel.js')(sequelize, Sequelize);
+// db.UserHistory = require('./userHistoryModel.js')(sequelize, Sequelize);
+
+
+// Define User-Agents junction table
+const UserAgent = sequelize.define('userAgent', {});
+
+// Define Agents-ServiceProvider junction table
+const AgentServiceProvider = sequelize.define('agentServiceProvider', {});
+
+// Define User-ServiceProvider junction table
+const UserServiceProvider = sequelize.define('userServiceProvider', {});
+
+
+// Define associations
+db.User.belongsToMany(db.Agents, { through: UserAgent,
+as: "Agents",
+  foreignKey: "UserID"});
+
+db.Agents.belongsToMany(db.User, { through: UserAgent, 
+  as: "User",
+  foreignKey: "agentBIN"});
+
+db.Agents.belongsToMany(db.ServiceProviders, { through: AgentServiceProvider,
+  as: "ServiceProviders",
+  foreignKey: "agentBIN" });
+
+/*db.ServiceProviders.belongsToMany(db.Agents, { through: AgentServiceProvider,
+  as: "Agents",
+  foreignKey: "serviceProviderBIN"});*/
+
+db.User.belongsToMany(db.ServiceProviders, { through: UserServiceProvider,
+  as: "ServiceProviders",
+  foreignKey: "UserID"});
+  
+/*db.ServiceProviders.belongsToMany(db.User, { through: UserServiceProvider,
+  as: "User",
+  foreignKey: "serviceProviderBIN"});*/
+
+db.Agents.hasMany(db.payment);
+db.payment.belongsTo(db.Agents);
+
+// db.AgentHistory.hasOne(db.payment);
+// db.payment.belongsTo(db.AgentHistory);
+
+db.User.hasMany(db.payment);
+db.payment.belongsTo(db.User);
+
+db.ServiceProviders.hasMany(db.payment);
+db.payment.belongsTo(db.ServiceProviders);
+
+// db.ServiceProviderHistory.hasOne(db.Payment);
+// db.Payment.belongsTo(db.ServiceProviderHistory);
+
+// db.User.hasMany(db.UserHistory);
+// db.UserHistory.belongsTo(db.User);
+
+// db.Payment.hasOne(db.UserHistory);
+// db.UserHistory.belongsTo(db.Payment);
+
+db.ServiceProviders.hasMany(db.Bill);
+db.Bill.belongsTo(db.ServiceProviders);
+
+db.payment.hasOne(db.Bill);
+db.Bill.belongsTo(db.payment);
+
+db.User.hasMany(db.Bill);
+db.Bill.belongsTo(db.User);
 
 module.exports = db;
