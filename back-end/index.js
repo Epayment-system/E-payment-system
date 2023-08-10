9c6062f3054aed0e608817fb0b8010ac89959b2f
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-10 21:46:16 +0100

    Revert "Developer 6"

--- back-end/index.js
+++ back-end/index.js
@@ -1,81 +1,15 @@
+
+
 // Import necessary packages and modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
+const express = require("express");
 const app = express();
+const db = require("./models");
 
-var corOptions = {
-  origin: 'https://localhost:8081'
-}
-
-//middleware
-app.use(cors(corOptions))
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-
-//
-const db = require('./models/index.js')
-db.sequelize.sync();
-
-db.sequelize.sync({force: false })
-.then(() => {
-  console.log('it is working');
-}); 
-
-// Import controllers
-const billController = require('./controller/billController.js')
-const serviceController = require('./controller/serviceProviderController.js')
-const paymentController = require('./controller/paymentController.js');
-const userController = require('./controller/userController.js');
-const AgentContoller = require('./controller/agentController.js');
-// Import routes
-const billsRouter = require('./routes/billRoute.js');
-const serviceProvidersRouter = require('./routes/serviceProviderRoute.js');
-const paymentRouter = require('./routes/paymentRoute.js');
-const usersRouter = require('./routes/userRoute.js');
-const AgentsRouter = require('./routes/agentRoute.js');
-
-// Mount routes
-app.use('/bill', billsRouter);
-app.use('/serviceprovider', serviceProvidersRouter);
-app.use('/payment', paymentRouter);
-app.use('/user', usersRouter);
-app.use('/agent', AgentsRouter);
-
-//testing api
-app.get('/',(req,res)=>{
-  res.json({message: 'hello there'})
-})
-app.post('/',(req,res)=>{
-  res.json({message: 'hello from post....'})
-})
-
-//Port
-const PORT = process.env.PORT || 3001
-
-// start server
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+// Call the sync() method on the Sequelize instance to synchronize the database schema with the Sequelize models
+db.sequelize.sync().then(() => {
+  // Start the server once the database has been synced successfully
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
-
-
-// up routes
-// app.use('/agents', agentController);
-// app.use('/bills', billController);
-// app.use('/payments', paymentController);
-// app.use('/services', serviceController);
-// app.use('/users', userController);
-// app.use('/agentHistory', agentHistoryController);
-// app.use('/serviceHistory', serviceHistoryController);
-// app.use('/userHistory', userHistoryController);
-
-// start server
-// app.listen(PORT, () => {
-//   console.log(Server started on port ${PORT});
-//   connection.connect(function(err){
-//     if (err) throw err;
-//     console.log('database connected');
-
-//   })
-// });
