fbb9a830cb136e27b068c12dd7396a7481dd6487
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-10 22:29:17 +0100

    routes

--- back-end/routes/agentRoute.js
+++ back-end/routes/agentRoute.js
@@ -1,20 +1,17 @@
 const express = require('express');
-
+const agentRouter = express.Router();
 const AgentController = require('../controllers/agentController.js');
 
-const agentRouter = express.Router();
 
-// Define routes for Agents
 agentRouter.post('/',AgentController.upload,AgentController.create)
-agentRouter.post('/login',AgentController.agentLogin)
+agentRouter.post('/login',AgentController.login)
+
 agentRouter.get('/', AgentController.findAll)
 agentRouter.get('/:id', AgentController.findOne)
 agentRouter.put('/:id', AgentController.update)
 agentRouter.delete('/:id', AgentController.delete)
 
-module.exports= agentRouter;
-
-
+module.exports= agentRouter
 
 
 
