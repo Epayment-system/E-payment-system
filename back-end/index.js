e49dffba1fa29318062931d8b30b47b2715f3344
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-08-21 15:07:08 +0100

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ var corOptions = {
 }
 
 //middleware
-app.use(cors(corOptions))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
