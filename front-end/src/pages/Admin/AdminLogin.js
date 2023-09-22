5834fa1243a3c18a008e3fd71fd40c43cc0eba78
eden zewdu <edenzewdu434@gmail.com>
2023-09-22 13:46:50 +0300

    admin updated

--- front-end/src/pages/Admin/AdminLogin.js
+++ front-end/src/pages/Admin/AdminLogin.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'antd';
+import { Form, Button, message } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './style.css'; // Import custom CSS file
 
 
+
 const AdminLogin = () => {
-  const [UserName, setUserName] = useState('');
+  const [Identifier, setIdentifier] = useState('');
   const [Password, setPassword] = useState('');
-  const [Email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const { adminId } = useParams();
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Move isLoggedIn state here
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [adminData, setAdminData] = useState([]);
 
-  const handleUsernameChange = (e) => {
-    setUserName(e.target.value);
+  const handleIdentifierChange = (e) => {
+    setIdentifier(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
@@ -31,7 +32,7 @@ const AdminLogin = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ UserName, Email, Password }),
+        body: JSON.stringify({ identifier: Identifier, Password: Password }),
       });
   
       const data = await response.json();
@@ -39,24 +40,35 @@ const AdminLogin = () => {
       if (response.ok) {
         if (data && data.token) {
           localStorage.setItem('adminToken', data.token);
-          setIsLoggedIn(true);
-          console.log('Admin logged in successfully');
-          console.log(`${data.token},${data.user.id}`);
-          navigate(`/admin/dashboard/${data.user.id}`);
+          localStorage.setItem('adminData', JSON.stringify(data.user)); // Store adminData as JSON
+  
+          // Check if the user has the admin role
+          if (data.user.Role === 'Admin') {
+            setIsLoggedIn(true);
+            console.log('Admin logged in successfully');
+            console.log(`${data.token},${data.user.id}`);
+            console.log(localStorage.getItem('adminData'));
+            navigate(`/Admin/Dashboard/${data.user.id}`);
+          } else {
+            message.error('You are not authorized to access the admin page.');
+            console.error('User does not have admin role');
+          }
         } else {
-          console.error('Invalid server response:', data);
+        message.error('Server error');
+      console.error('Invalid server response:', data);
         }
       } else {
+        message.error('Admin login failed');
+        message.error('Insert valid username and password');
         console.error('Admin login failed:', data.error);
       }
     } catch (error) {
+      message.error('An error occurred during admin login');
       console.error('An error occurred during admin login:', error);
     }
   
     setLoading(false);
-  
-    setUserName('');
-    setEmail('');
+    setIdentifier('');
     setPassword('');
   };
 
@@ -64,17 +76,17 @@ const AdminLogin = () => {
     <div className="login">
       <h1>Login</h1>
       <Form className="form" onFinish={handleSubmit}>
-        <label htmlFor="username">
+        <label htmlFor="identifier">
           <UserOutlined className="icon" />
         </label>
         <input
           type="text"
-          name="username"
+          name="identifier"
           placeholder="Username or Email"
-          id="username"
+          id="identifier"
           required
-          value={UserName}
-          onChange={handleUsernameChange}
+          value={Identifier}
+          onChange={handleIdentifierChange}
         />
         <label htmlFor="password">
           <LockOutlined className="icon" />
@@ -89,7 +101,7 @@ const AdminLogin = () => {
           onChange={handlePasswordChange}
         />
         <Button className="button" type="primary" htmlType="submit" loading={loading} disabled={loading}>
-          Login
+          {loading ? 'Logging in...' : 'Login'}
         </Button>
       </Form>
     </div>
@@ -97,3 +109,6 @@ const AdminLogin = () => {
 };
 
 export default AdminLogin;
+
+
+
